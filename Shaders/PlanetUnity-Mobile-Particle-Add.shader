14ce41bad5dc2922bbdf94a96a188f25be5c1602
Rocco Bowling <rocco@chimerasw.com>
2016-09-08 11:46:32 -0400

    Initial version of PUParticles, an entity which references an existing Unity shiruken particle system and copies its geometry over to Unity UI geometry, allowing for truly embedded particles in Unity UI.  Make sure there is an existing shiruken partcile system in the scene, and set the systemName attirbute on <Particles> to be the name of he GameObject of that particle system.  Default settings will have the particle emitter scale up/down to fill the RectTransform of the PUParticles entity,  Supports color change over lifetime, size over lifetime, and texture sheet animations.

---
+++ Shaders/PlanetUnity-Mobile-Particle-Add.shader
@@ -0,0 +1,102 @@
+// Planet Unity:
+// - Position, rotation, and size computed in vertex shader
+
+Shader "PlanetUnity/Mobile/Particles/Additive" {
+Properties
+	{
+		[PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+		_Color ("Tint", Color) = (1,1,1,1)
+
+		_ScaleInfo ("Scale Info", Vector) = (1,1,1,1)
+		
+		_StencilComp ("Stencil Comparison", Float) = 8
+		_Stencil ("Stencil ID", Float) = 0
+		_StencilOp ("Stencil Operation", Float) = 0
+		_StencilWriteMask ("Stencil Write Mask", Float) = 255
+		_StencilReadMask ("Stencil Read Mask", Float) = 255
+		
+		_ColorMask ("Color Mask", Float) = 15
+	}
+
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+		
+		Stencil
+		{
+			Ref [_Stencil]
+			Comp [_StencilComp]
+			Pass [_StencilOp] 
+			ReadMask [_StencilReadMask]
+			WriteMask [_StencilWriteMask]
+		}
+
+		Cull Back
+		Lighting Off
+		ZWrite Off
+		ZTest [unity_GUIZTestMode]
+		Fog { Mode Off }
+		Blend SrcAlpha One
+		ColorMask [_ColorMask]
+
+		Pass
+		{
+		CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			struct appdata_t
+			{
+				float4 vertex   : POSITION;
+				float4 color    : COLOR;
+				float4 normal    : NORMAL;
+				float4 tangent    : TANGENT;
+				float2 texcoord0 : TEXCOORD0;
+				float2 texcoord1 : TEXCOORD1;
+			};
+
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+				fixed4 color    : COLOR;
+				half2 texcoord  : TEXCOORD0;
+			};
+			
+			fixed4 _Color;
+			float4 _ScaleInfo;
+
+			v2f vert(appdata_t IN)
+			{
+				v2f OUT;
+
+				// NORMAL xy contains x and y offset from IN.vertex
+				// texcoord1 x contains size of the offset
+				// texcoord1 y contains rotation in radians
+				float c = cos (IN.texcoord1.y * 0.01745329);
+				float s = sin (IN.texcoord1.y * 0.01745329);
+				OUT.vertex = mul(UNITY_MATRIX_MVP, (IN.vertex * float4(_ScaleInfo.xy, 1, 1) + float4(IN.normal.x * IN.texcoord1.x * c - IN.normal.y * IN.texcoord1.x * s, IN.normal.x * IN.texcoord1.x * s + IN.normal.y * IN.texcoord1.x * c, 0, 0) * (_ScaleInfo.x + _ScaleInfo.y) * 0.5));
+
+				OUT.texcoord = IN.texcoord0;
+				OUT.color = IN.color * _Color;
+				return OUT;
+			}
+
+			sampler2D _MainTex;
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				return tex2D(_MainTex, IN.texcoord) * IN.color;
+			}
+		ENDCG
+		}
+	}
+
+}
---
+++ Source/PUParticles.cs
@@ -0,0 +1,305 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public partial class PUParticles : PUParticlesBase {
+
+	ParticleSystemRenderer particleRenderer;
+	ParticleSystem particleSystem;
+	ParticleSystem.Particle[] particleArray;
+
+	private ParticleSystem.TextureSheetAnimationModule textureSheetAnimation;
+	private int textureSheetAnimationFrames;
+	private Vector2 textureSheedAnimationFrameSize;
+
+	private const int lutMax = 100;
+	private Color[] colorLUT = new Color[lutMax];
+	private float[] sizeLUT = new float[lutMax];
+	private Vector4[] sheetLUT = new Vector4[lutMax];
+
+	private int designedMaxParticles;
+
+
+	public override void gaxb_init () {
+		base.gaxb_init ();
+		gameObject.name = "<Particles/>";
+	}
+
+
+	public override void gaxb_complete() {
+		if (systemName != null) {
+			GameObject particleSystemGO = GameObject.Find (systemName);
+			if (particleSystemGO != null) {
+				particleSystem = particleSystemGO.GetComponent<ParticleSystem> ();
+				particleRenderer = particleSystemGO.GetComponent<ParticleSystemRenderer> ();
+
+				shim.material = particleRenderer.material;
+
+				particleSystem.simulationSpace = ParticleSystemSimulationSpace.Local;
+				particleRenderer.enabled = false;
+
+				designedMaxParticles = particleSystem.maxParticles;
+
+				// create the LUTs for less CPU usages during mesh generation
+				for (int i = 0; i < lutMax; i++) {
+					colorLUT [i] = particleSystem.startColor;
+					sizeLUT [i] = particleSystem.startSize;
+				}
+
+				// support changing color over lifetime
+				if (particleSystem.colorOverLifetime.enabled) {
+					for (int i = 0; i < lutMax; i++) {
+						colorLUT [i] = particleSystem.colorOverLifetime.color.Evaluate ((float)i / (float)lutMax);
+					}
+				}
+
+				// support changing size over lifetime
+				if (particleSystem.sizeOverLifetime.enabled) {
+					for (int i = 0; i < lutMax; i++) {
+						sizeLUT [i] = particleSystem.sizeOverLifetime.size.Evaluate ((float)i / (float)lutMax);
+					}
+				}
+					
+
+				// prepare texture sheet animation
+				textureSheetAnimation = particleSystem.textureSheetAnimation;
+				textureSheetAnimationFrames = 0;
+				textureSheedAnimationFrameSize = Vector2.zero;
+				if (textureSheetAnimation.enabled) {
+					textureSheetAnimationFrames = textureSheetAnimation.numTilesX * textureSheetAnimation.numTilesY;
+					textureSheedAnimationFrameSize = new Vector2(1f / textureSheetAnimation.numTilesX, 1f / textureSheetAnimation.numTilesY);
+
+					// support texture sheet animations changing over lifetime
+					for (int i = 0; i < lutMax; i++) {
+						float frameProgress = (float)i / (float)lutMax;
+						frameProgress = Mathf.Repeat ((1.0f - frameProgress) * textureSheetAnimation.cycleCount, 1);
+						int frame = 0;
+
+						switch (textureSheetAnimation.animation) {
+
+						case ParticleSystemAnimationType.WholeSheet:
+							frame = Mathf.FloorToInt (frameProgress * textureSheetAnimationFrames);
+							break;
+
+						case ParticleSystemAnimationType.SingleRow:
+							frame = Mathf.FloorToInt (frameProgress * textureSheetAnimation.numTilesX);
+
+							int row = textureSheetAnimation.rowIndex;
+							frame += row * textureSheetAnimation.numTilesX;
+							break;
+						}
+
+						frame %= textureSheetAnimationFrames;
+
+						float x = (frame % textureSheetAnimation.numTilesX) * textureSheedAnimationFrameSize.x;
+						float y = Mathf.FloorToInt (frame / textureSheetAnimation.numTilesX) * textureSheedAnimationFrameSize.y;
+						float x2 = x + textureSheedAnimationFrameSize.x;
+						float y2 = y - textureSheedAnimationFrameSize.y;
+
+						sheetLUT [i] = new Vector4 (x, y, x2, y2);
+					}
+				}
+
+
+			}
+
+		}
+
+		ScheduleForUpdate ();
+	}
+
+	private int skipFramesMax;
+	private int skipFramesCounter;
+
+	private int frameCount = 0;
+	private float dt = 0.0f;
+	private float fps = 0.0f;
+	private float updateRate = 2.0f;
+
+
+	public override void Update() {
+		if (particleSystem != null) {
+			
+
+			if (adjustToFPS) {
+				const float fpsCutoff = 50.0f;
+				const int maxFrameSkipAllowed = 5;
+
+				// Get a running average of FPS to know if we should adjust things
+				frameCount++;
+				dt += Time.deltaTime;
+				if (dt > 1.0f / updateRate) {
+					fps = frameCount / dt;
+					frameCount = 0;
+					dt -= 1.0f / updateRate;
+				
+					int particleAdjust = Mathf.CeilToInt (particleSystem.maxParticles * 0.02f);
+
+					if (fps < fpsCutoff) {
+						skipFramesMax++;
+						particleSystem.maxParticles -= particleAdjust;
+					} else {
+						skipFramesMax--;
+						particleSystem.maxParticles += particleAdjust;
+					}
+
+					if (skipFramesMax < 1) {
+						skipFramesMax = 1;
+					}
+					if (skipFramesMax > maxFrameSkipAllowed) {
+						skipFramesMax = maxFrameSkipAllowed;
+					}
+
+					if (particleSystem.maxParticles > designedMaxParticles) {
+						particleSystem.maxParticles = designedMaxParticles;
+					}
+					if (particleSystem.maxParticles < designedMaxParticles / 4) {
+						particleSystem.maxParticles = designedMaxParticles / 4;
+					}
+
+					// We skip frames first; if that doesn't work, then we lower
+					// the max number of particles in the system
+					if (particleSystem.maxParticles < designedMaxParticles) {
+						skipFramesMax = maxFrameSkipAllowed;
+					}
+				}
+
+				skipFramesCounter--;
+				if (skipFramesCounter > 0) {
+					return;
+				}
+				skipFramesCounter = skipFramesMax;
+			}
+
+			//Debug.Log (string.Format ("***** {0} :: {1} fps :: {2} skips", particleSystem.maxParticles, fps, skipFramesMax));
+			SetVerticesDirty ();
+		}
+	}
+
+	public override void OnPopulateMesh(VertexHelper vh) {
+
+		vh.Clear ();
+
+		if (particleSystem == null) {
+			return;
+		}
+
+		// do we need to allocate / resize our particle array?
+		if (particleArray == null) {
+			particleArray = new ParticleSystem.Particle[particleSystem.maxParticles];
+		}
+		if (particleArray.Length < particleSystem.maxParticles) {
+			Array.Resize (ref particleArray, particleSystem.maxParticles);
+		}
+
+		float shapeSizeX = 1.0f;
+		float shapeSizeY = 1.0f;
+
+		if (scaleToFit) {
+			if (particleSystem.shape.shapeType == ParticleSystemShapeType.Box) {
+				shapeSizeX = particleSystem.shape.box.x;
+				shapeSizeY = particleSystem.shape.box.y;
+			} else if (particleSystem.shape.shapeType == ParticleSystemShapeType.Sphere ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.SphereShell ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.Hemisphere ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.HemisphereShell ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.Cone ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.ConeShell ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.ConeVolume ||
+			           particleSystem.shape.shapeType == ParticleSystemShapeType.ConeVolumeShell) {
+				shapeSizeX = particleSystem.shape.radius * 2.0f;
+				shapeSizeY = particleSystem.shape.radius * 2.0f;
+			}
+		} else if (customScale != null) {
+			shapeSizeX = customScale.Value.x;
+			shapeSizeY = customScale.Value.y;
+		}
+
+		int liveParticleCount = particleSystem.GetParticles (particleArray);
+		float scaleX = rectTransform.rect.width / shapeSizeX;
+		float scaleY = rectTransform.rect.height / shapeSizeY;
+
+		float avgScale = (scaleX + scaleY) * 0.5f;
+
+		if (particleRenderer.material.HasProperty("_ScaleInfo")) {
+			
+			// special vertex shader which moves some calculations off of the CPU
+			particleRenderer.material.SetVector ("_ScaleInfo", new Vector4 (scaleX, scaleY, 0, 0));
+
+			UIVertex[] quad = new UIVertex[4] {
+				UIVertex.simpleVert,
+				UIVertex.simpleVert,
+				UIVertex.simpleVert,
+				UIVertex.simpleVert
+			};
+
+			quad [0].uv0 = new Vector2 (0f, 0f);
+			quad [1].uv0 = new Vector2 (0f, 1f);
+			quad [2].uv0 = new Vector2 (1f, 1f);
+			quad [3].uv0 = new Vector2 (1f, 0f);
+
+			quad [0].normal = new Vector3 (-1.0f, -1.0f, 0.0f);
+			quad [1].normal = new Vector3 (-1.0f, 1.0f, 0.0f);
+			quad [2].normal = new Vector3 (1.0f, 1.0f, 0.0f);
+			quad [3].normal = new Vector3 (1.0f, -1.0f, 0.0f);
+
+
+			for (int i = 0; i < liveParticleCount; i++) {
+				ParticleSystem.Particle p = particleArray [i];
+
+				float frameProgress = (p.lifetime / p.startLifetime);
+				int lutIdx = Mathf.FloorToInt (frameProgress * (lutMax - 1));
+
+				if (textureSheetAnimation.enabled) {
+
+					Vector4 uvs = sheetLUT[lutIdx];
+					
+					quad [0].uv0.x = uvs.x;
+					quad [0].uv0.y = uvs.w;
+					quad [1].uv0.x = uvs.x;
+					quad [1].uv0.y = uvs.y;
+					quad [2].uv0.x = uvs.z;
+					quad [2].uv0.y = uvs.y;
+					quad [3].uv0.x = uvs.z;
+					quad [3].uv0.y = uvs.w;
+				}
+
+				quad [0].position = quad [1].position = quad [2].position = quad [3].position = p.position;
+				quad [0].color = quad [1].color = quad [2].color = quad [3].color = colorLUT [lutIdx];
+				quad [0].uv1 = quad [1].uv1 = quad [2].uv1 = quad [3].uv1 = new Vector2 (sizeLUT [lutIdx], p.rotation);
+
+				vh.AddUIVertexQuad (quad);
+			}
+
+		} else {
+
+			for (int i = 0; i < liveParticleCount; i++) {
+				ParticleSystem.Particle p = particleArray [i];
+
+				Vector3 pos = p.position;
+				float sizeX = p.GetCurrentSize (particleSystem) / 2.0f;
+				float sizeY = sizeX;
+				Color color = p.GetCurrentColor (particleSystem);
+				float rotation = p.rotation * Mathf.Deg2Rad;
+
+				pos.x *= scaleX;
+				pos.y *= scaleY;
+
+				sizeX *= avgScale;
+				sizeY *= avgScale;
+
+				int idx = vh.currentVertCount;
+
+				vh.AddVert (pos + new Vector3 (-sizeX, -sizeY).RotateZ (rotation), color, new Vector2 (0f, 0f));
+				vh.AddVert (pos + new Vector3 (-sizeX, +sizeY).RotateZ (rotation), color, new Vector2 (0f, 1f));
+				vh.AddVert (pos + new Vector3 (+sizeX, +sizeY).RotateZ (rotation), color, new Vector2 (1f, 1f));
+				vh.AddVert (pos + new Vector3 (+sizeX, -sizeY).RotateZ (rotation), color, new Vector2 (1f, 0f));
+
+				vh.AddTriangle (idx + 0, idx + 1, idx + 2);
+				vh.AddTriangle (idx + 2, idx + 3, idx + 0);
+			}
+		}
+
+	}
+
+}
