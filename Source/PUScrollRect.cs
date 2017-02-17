6188f820b3da639076a3e9f76af58a3d32307727
Taylor Plimpton <taylorplimpton@Taylors-MBP.home>
2017-02-17 02:40:08 -0500

    ScrollRect Content Min/Max
    - Taking content offset into account

--- Source/PUScrollRect.cs
+++ Source/PUScrollRect.cs
@@ -168,8 +168,8 @@ public partial class PUScrollRect : PUScrollRectBase {
 			//maxX = 0;
 		}
 
-		_contentMin = new Vector2 (minX, minY);
-		_contentMax = new Vector2 (maxX, maxY);
+		_contentMin = new Vector2 (minX - _ContentOffset.x, minY - _ContentOffset.y);
+		_contentMax = new Vector2 (maxX - _ContentOffset.x, maxY - _ContentOffset.y);
 
 		myRectTransform.sizeDelta = new Vector2 (Mathf.Abs(maxX - minX) + _ContentOffset.x, Mathf.Abs(maxY - minY) + _ContentOffset.y);
 	}
